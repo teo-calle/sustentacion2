8f378c71ed9b40f26d8a47fd6ce4ba3d232842d3
MAC <mac@MacBook-Pro-de-MAC.local>
2016-03-07 17:49:09 -0500

    Initial commit

---
+++ app/src/main/java/com/teo/mac/registro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.teo.mac.registro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
